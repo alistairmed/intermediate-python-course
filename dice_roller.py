dc9e2fd9b304f35bbd7a20be348f6e3d068b458f
Alistair_Medical Student <68220428+alistairmed@users.noreply.github.com>
2020-07-19 00:10:39 +0800

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,15 @@
 import random 
 def main():
+<<<<<<< HEAD
  roll = random.randint(1,6)
  print(f'You rolled a {roll}')
 
 if __name__== "__main__":
   main()
 
+=======
+  #print('You rolled a die')
+
+if __name__== "__main__":
+  main()
+>>>>>>> parent of 16884ce... Update dice_roller.py
